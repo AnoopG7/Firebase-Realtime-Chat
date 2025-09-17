bcc81300a45dc9bd0e57c93e8ecf534cb8b2c7b4
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-09-17 15:17:33 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+//config firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyA1hi40suOkUIrmtKpCJnEnAtMsqOvUOEY",
+  authDomain: "class-6a3ae.firebaseapp.com",
+  projectId: "class-6a3ae",
+  storageBucket: "class-6a3ae.firebasestorage.app",
+  messagingSenderId: "993948361196",
+  appId: "1:993948361196:web:10573bcc739276eb2ea0c9",
+  measurementId: "G-V2YRBG9HSX",
+};
+
